69e1c5b0ed41b903e9037111a7acd2d7f3ded8b6
depayse <cbs5295@gmail.com>
2023-04-06 16:20:39 +0900

    [DOCS] MVC Activity 주석 작성 및 View 코드 리팩토링

--- app/src/main/java/com/depayse/architecturepattern/mvc/controller/MVCActivity.kt
+++ app/src/main/java/com/depayse/architecturepattern/mvc/controller/MVCActivity.kt
@@ -12,8 +12,24 @@ import com.depayse.architecturepattern.R
 import com.depayse.architecturepattern.databinding.ActivityMvcBinding
 import com.depayse.architecturepattern.mvc.model.Board
 
+/**
+ * Android 에서 MVC 를 구현할 때 Activity 가 Controller 가 된다.
+ *
+ * [MVCActivity]는 MVC 패턴에서 Controller 역할을 한다.
+ *  ```
+ *  ```
+ * In android, Activity becomes Controller when implementing MVC Pattern.<br/><br/>
+ *
+ * [MVCActivity] is responsible for Controller in MVC Pattern.<br/><br/>
+ */
 class MVCActivity : AppCompatActivity() {
 
+    /**
+     * Controller 는 Model 과 View 에 대해 알고 있다.
+     *  ```
+     *  ```
+     *  Controller does know about Model and View.
+     */
     private lateinit var model: Board
     private lateinit var viewBinding: ActivityMvcBinding
 
@@ -21,8 +37,27 @@ class MVCActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMvcBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+        setOnCellClickListener()
 
         model = Board()
+    }
+
+    /**
+     * Controller 는 입력을 받는 역할을 한다.
+     *
+     * [setOnCellClickListener] 는 Controller 가 보드의 셀 하나를 클릭하는 입력을 받도록 해준다.
+     * ```
+     * ```
+     * Controller takes user input.
+     *
+     * [setOnCellClickListener] makes Controller take input of a single cell of a board.
+     */
+    private fun setOnCellClickListener() {
+        viewBinding.tableBoard.children.forEach { tableRowView ->
+            (tableRowView as? TableRow)?.children?.forEach { buttonView ->
+                buttonView.setOnClickListener { onCellClicked(buttonView) }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -30,6 +65,16 @@ class MVCActivity : AppCompatActivity() {
         return true
     }
 
+    /**
+     * Controller 는 입력을 받는 역할을 한다.
+     *
+     * [onOptionsItemSelected] 는 Controller 가 초기화 옵션을 클릭하는 입력을 받도록 해준다.
+     * ```
+     * ```
+     * Controller takes user input.
+     *
+     * [onOptionsItemSelected] makes Controller take input of a reset option.
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             R.id.action_reset -> {
@@ -42,6 +87,16 @@ class MVCActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Controller 는 사용자의 입력을 받고, Model 과 View 의 변경을 모니터링하고, 업데이트한다.
+     *
+     * [onCellClicked] 는 사용자가 셀을 클릭하는 입력이 일어난 후에 Model 과 View 를 업데이트한다.
+     * ```
+     * ```
+     * Controller takes user input, monitors changes of Model and View, updates Model and View.
+     *
+     * [onCellClicked] updates Model and View after user clicks a single cell.
+     */
     fun onCellClicked(v: View) {
         (v as? Button)?.let { button ->
             val tableRow = viewBinding.tableBoard.children.find { (it as? TableRow)?.children?.contains(v) == true } as? TableRow
@@ -58,6 +113,16 @@ class MVCActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Controller 는 사용자의 입력을 받고, Model 과 View 의 변경을 모니터링하고, 업데이트한다.
+     *
+     * [reset] 은 사용자가 reset 옵션을 클릭하는 입력이 일어난 후에 Model 과 View 를 초기화한다.
+     * ```
+     * ```
+     * Controller takes user input, monitors changes of Model and View, updates Model and View.
+     *
+     * [reset] function resets Model and View after user clicks reset option.
+     */
     private fun reset() {
         viewBinding.winnerPlayerViewGroup.visibility = View.GONE
         viewBinding.winnerPlayerLabel.text = ""
